353bf43e85e4d6b1d9434d06d73e044d63ff3e12
Ayoola Akindolani <akindolaniayoola@gmail.com>
2020-09-26 02:52:50 +0100

    new modofication

--- src/components/screens/order-management/OrderItem.js
+++ src/components/screens/order-management/OrderItem.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { format } from "date-fns";
 
-import ModalComponent from 'components/shared/modal/ModalComponent';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 
 import { orderActions } from "state/actions/orderActions";
 
@@ -46,7 +50,26 @@ const OrderItem = ({ order }) => {
     <>
       <tr>
         <td style={{ padding: '1rem 1rem 1rem 0' }}>
-          <ModalComponent id={id} operation={operation} stat={open} />
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Are you sure you want to {operation}?
+          </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={approve} color="primary" autoFocus>
+                Yes
+          </Button>
+              <Button onClick={reject} color="primary">
+                No
+          </Button>
+            </DialogActions>
+          </Dialog>
         </td>
         <td>{orderDate}</td>
         <td>{order.order.orderNo}</td>
