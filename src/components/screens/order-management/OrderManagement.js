0a0c908920386d1135da59dfd5b32c469de47ee0
Ayoola Akindolani <akindolaniayoola@gmail.com>
2020-09-28 09:04:30 +0100

    fix bug

--- src/components/screens/order-management/OrderManagement.js
+++ src/components/screens/order-management/OrderManagement.js
@@ -28,13 +28,13 @@ const OrderManagement = () => {
     <div>
       <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
-        {pending ? <Spinner /> : null}
         <HeaderNav />
         <LeftNav />
         <div className="page-wrapper" style={{ display: 'block' }}>
           <div className="col-12">
             <div className="card">
-              <BreadCrumb title="Order Management" isAdmin="neutral"/>
+              <BreadCrumb title="Order Management" isAdmin="neutral" />
+              {pending ? <Spinner /> : null}
               <div className="table-responsive">
                 <table className="table table-striped mb-0">
                   <thead className="bg-primary text-white">
